5dd3c91373630ec0be3b02b07b1b63c3ca153e20
Manpreet Kaur <manpreet.kaur@marblex.ai>
2026-02-03 16:16:56 +0530

    removed ex

--- ex.py
+++
@@ -1,7 +0,0 @@
-from nltk.stem import PorterStemmer,RegexpStemmer
-# words=["eating","eaten","eat","finally","finalize","goes","gone","going","writing","writes","programming","programs"]
-# stemming=PorterStemmer()
-# for word in words:
-#     print(word+"------>"+stemming.stem(word))
-regStemmer=RegexpStemmer("ing|s$|e$|ed$|es$")
-print(regStemmer.stem("ingeating"))
